dcf947b254ff9ff8ebd626d800581f458735e2a1
Emirhan TALA <tala.emirhan@gmail.com>
2024-10-13 16:31:50 +0300

    feat: move not suggested `range` functions in "test_only" feature

--- src/keys_and_address.rs
+++ src/keys_and_address.rs
@@ -20,13 +20,10 @@
 //!                 random_address.get_comp_address())
 //! ```
 
-use crate::error::BtcVanityError;
 use bitcoin::key::{PrivateKey, PublicKey};
 use bitcoin::secp256k1::{rand, All, Secp256k1};
 use bitcoin::Address;
 use bitcoin::Network::Bitcoin;
-use num_bigint::{BigUint, RandBigInt};
-use num_traits::Num;
 
 /// A struct to hold bitcoin::secp256k1::SecretKey bitcoin::Key::PublicKey and a string address
 pub struct KeysAndAddress {
@@ -36,7 +33,7 @@ pub struct KeysAndAddress {
 }
 
 impl KeysAndAddress {
-    /// Generates a randomly generated key pair and their compressed addresses without generating a new Secp256k1.
+    /// Generates a randomly generated key pair and their compressed addresses with using given Secp256k1.
     /// and Returns them in a KeysAndAddress struct.
     pub fn generate_random(secp256k1: &Secp256k1<All>) -> Self {
         let (secret_key, pk) = secp256k1.generate_keypair(&mut rand::thread_rng());
@@ -65,111 +62,6 @@ impl KeysAndAddress {
         }
     }
 
-    /// Use safe mode if you're calling this function out of vanity_addr_generator.rs
-    /// Generates a randomly generated key pair and their compressed addresses within a custom range for the private key.
-    /// Returns them in a KeysAndAddress struct.
-    /// The range is defined by `range_min` and `range_max` as BigUint to handle 256-bit values.
-    pub fn generate_within_range(
-        s: &Secp256k1<All>,
-        range_min: &BigUint,
-        range_max: &BigUint,
-        safe_mode: bool,
-    ) -> Result<Self, BtcVanityError> {
-        if safe_mode {
-            // Ensure range_max is greater than range_min
-            if range_max < range_min {
-                return Err(BtcVanityError::KeysAndAddressError(
-                    "range_max must be greater than range_min",
-                ));
-            }
-
-            let secp256k1_order = BigUint::from_str_radix(
-                "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
-                16,
-            )
-            .map_err(|_| {
-                BtcVanityError::KeysAndAddressError("Failed to parse hexadecimal string")
-            })?;
-
-            if range_max > &secp256k1_order {
-                return Err(BtcVanityError::KeysAndAddressError(
-                    "range_max must be within the valid range for Secp256k1",
-                ));
-            }
-        }
-
-        // Generate a random private key within the specified range
-        let mut rng = rand::thread_rng();
-        let private_key_value = rng.gen_biguint_range(range_min, range_max);
-
-        // Convert the BigUint to a 32-byte array, zero-padded on the left
-        let private_key_bytes = {
-            let mut bytes = [0u8; 32];
-            let private_key_vec = private_key_value.to_bytes_be();
-            let start_index = 32 - private_key_vec.len();
-            bytes[start_index..].copy_from_slice(&private_key_vec);
-            bytes
-        };
-
-        let private_key = PrivateKey::from_slice(&private_key_bytes, Bitcoin)
-            .map_err(|_| BtcVanityError::KeysAndAddressError("Invalid private key"))?;
-        let public_key = PublicKey::from_private_key(s, &private_key);
-
-        Ok(KeysAndAddress {
-            private_key,
-            public_key,
-            comp_address: Address::p2pkh(public_key, Bitcoin).to_string(),
-        })
-    }
-
-    /// Use safe mode if you're calling this function out of vanity_addr_generator.rs
-    /// Generates a the key pair and their compressed addresses from the given private key.
-    /// Returns them in a KeysAndAddress struct.
-    pub fn generate_from_biguint(
-        s: &Secp256k1<All>,
-        private_key_biguint: &BigUint,
-        safe_mode: bool,
-    ) -> Result<Self, BtcVanityError> {
-        if safe_mode {
-            if private_key_biguint == &BigUint::ZERO {
-                return Err(BtcVanityError::KeysAndAddressError("renge_min can't be 0"));
-            }
-
-            let secp256k1_order = BigUint::from_str_radix(
-                "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
-                16,
-            )
-            .map_err(|_| {
-                BtcVanityError::KeysAndAddressError("Failed to parse hexadecimal string")
-            })?;
-
-            if private_key_biguint > &secp256k1_order {
-                return Err(BtcVanityError::KeysAndAddressError(
-                    "range_max must be within the valid range for Secp256k1",
-                ));
-            }
-        }
-
-        // Convert the BigUint to a 32-byte array, zero-padded on the left
-        let private_key_bytes = {
-            let mut bytes = [0u8; 32];
-            let private_key_vec = private_key_biguint.to_bytes_be();
-            let start_index = 32 - private_key_vec.len();
-            bytes[start_index..].copy_from_slice(&private_key_vec);
-            bytes
-        };
-
-        let private_key = PrivateKey::from_slice(&private_key_bytes, Bitcoin)
-            .map_err(|_| BtcVanityError::KeysAndAddressError("Invalid private key"))?;
-        let public_key = PublicKey::from_private_key(s, &private_key);
-
-        Ok(KeysAndAddress {
-            private_key,
-            public_key,
-            comp_address: Address::p2pkh(public_key, Bitcoin).to_string(),
-        })
-    }
-
     pub fn get_private_key(&self) -> &PrivateKey {
         &self.private_key
     }
@@ -191,12 +83,137 @@ impl KeysAndAddress {
     }
 }
 
+#[cfg(feature = "test_only")]
+mod test_only_features {
+    use super::*;
+    use crate::error::BtcVanityError;
+    use bitcoin::secp256k1::Secp256k1;
+    use num_bigint::BigUint;
+    use num_bigint::RandBigInt;
+    use num_traits::Num;
+
+    impl KeysAndAddress {
+        /// USE ONLY FOR TESTING. USING THIS FUNCTION FOR CREATING A KEY PAIR IS NOT SAFE!!!
+        ///
+        /// Use safe mode if you're calling this function out of vanity_addr_generator.rs
+        /// Safe mode checks if the range is valid or not.
+        /// Generates a randomly generated key pair and their compressed addresses within a custom range for the private key.
+        /// The range is defined by `range_min` and `range_max` as BigUint to handle 256-bit values.
+        /// Returns a Result
+        /// `Ok`, returns KeysAndAddress struct of a random private key within given range.
+        /// `Err` returns BtcVanityError if the range or randomly generated private key is invalid
+        pub fn generate_within_range(
+            s: &Secp256k1<All>,
+            range_min: &BigUint,
+            range_max: &BigUint,
+            safe_mode: bool,
+        ) -> Result<Self, BtcVanityError> {
+            if safe_mode {
+                // Ensure range_max is greater than range_min
+                if range_max < range_min {
+                    return Err(BtcVanityError::KeysAndAddressError(
+                        "range_max must be greater than range_min",
+                    ));
+                }
+
+                let secp256k1_order = BigUint::from_str_radix(
+                    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
+                    16,
+                )
+                .map_err(|_| {
+                    BtcVanityError::KeysAndAddressError("Failed to parse hexadecimal string")
+                })?;
+
+                if range_max > &secp256k1_order {
+                    return Err(BtcVanityError::KeysAndAddressError(
+                        "range_max must be within the valid range for Secp256k1",
+                    ));
+                }
+            }
+
+            // Generate a random private key within the specified range
+            let mut rng = rand::thread_rng();
+            let private_key_value = rng.gen_biguint_range(range_min, range_max);
+
+            // Convert the BigUint to a 32-byte array, zero-padded on the left
+            let private_key_bytes = {
+                let mut bytes = [0u8; 32];
+                let private_key_vec = private_key_value.to_bytes_be();
+                let start_index = 32 - private_key_vec.len();
+                bytes[start_index..].copy_from_slice(&private_key_vec);
+                bytes
+            };
+
+            let private_key = PrivateKey::from_slice(&private_key_bytes, Bitcoin)
+                .map_err(|_| BtcVanityError::KeysAndAddressError("Invalid private key"))?;
+            let public_key = PublicKey::from_private_key(s, &private_key);
+
+            Ok(KeysAndAddress {
+                private_key,
+                public_key,
+                comp_address: Address::p2pkh(public_key, Bitcoin).to_string(),
+            })
+        }
+
+        /// USE ONLY FOR TESTING. USING THIS FUNCTION FOR CREATING A KEY PAIR IS NOT SAFE!!!
+        ///
+        /// Use safe mode if you're calling this function out of vanity_addr_generator.rs
+        /// Safe mode checks if the range is valid or not.
+        /// Generates a key pair and their compressed addresses from the given private key.
+        /// Returns a Result
+        /// `Ok`, returns KeysAndAddress struct of given private key.
+        /// `Err` returns BtcVanityError if the given private key is invalid
+        pub fn generate_from_biguint(
+            s: &Secp256k1<All>,
+            private_key_biguint: &BigUint,
+            safe_mode: bool,
+        ) -> Result<Self, BtcVanityError> {
+            if safe_mode {
+                if private_key_biguint == &BigUint::ZERO {
+                    return Err(BtcVanityError::KeysAndAddressError("renge_min can't be 0"));
+                }
+
+                let secp256k1_order = BigUint::from_str_radix(
+                    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
+                    16,
+                )
+                .map_err(|_| {
+                    BtcVanityError::KeysAndAddressError("Failed to parse hexadecimal string")
+                })?;
+
+                if private_key_biguint > &secp256k1_order {
+                    return Err(BtcVanityError::KeysAndAddressError(
+                        "range_max must be within the valid range for Secp256k1",
+                    ));
+                }
+            }
+
+            // Convert the BigUint to a 32-byte array, zero-padded on the left
+            let private_key_bytes = {
+                let mut bytes = [0u8; 32];
+                let private_key_vec = private_key_biguint.to_bytes_be();
+                let start_index = 32 - private_key_vec.len();
+                bytes[start_index..].copy_from_slice(&private_key_vec);
+                bytes
+            };
+
+            let private_key = PrivateKey::from_slice(&private_key_bytes, Bitcoin)
+                .map_err(|_| BtcVanityError::KeysAndAddressError("Invalid private key"))?;
+            let public_key = PublicKey::from_private_key(s, &private_key);
+
+            Ok(KeysAndAddress {
+                private_key,
+                public_key,
+                comp_address: Address::p2pkh(public_key, Bitcoin).to_string(),
+            })
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use bitcoin::secp256k1::Secp256k1;
-    use num_bigint::BigUint;
-    use num_traits::{FromPrimitive, One};
 
     #[test]
     fn test_generate_random() {
@@ -228,6 +245,15 @@ mod tests {
         let derived_address = Address::p2pkh(&derived_public_key, Bitcoin).to_string();
         assert_eq!(keys_and_address.comp_address, derived_address);
     }
+}
+
+#[cfg(test)]
+#[cfg(feature = "test_only")]
+mod test_only_tests {
+    use super::*;
+    use bitcoin::secp256k1::Secp256k1;
+    use num_bigint::BigUint;
+    use num_traits::{FromPrimitive, Num, One};
 
     #[test]
     fn test_generate_with_custom_range() {
